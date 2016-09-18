c8654ca450354c968b4080606a1066f9d85ecf68
dc004 <dc004@yangpengfei.lan>
2016-09-18 09:31:57 +0800

    Initial Commit

---
+++ 918第一个仓库/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  918第一个仓库
+//
+//  Created by dc004 on 16/9/18.
+//  Copyright © 2016年 dc004. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 918第一个仓库/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  918第一个仓库
+//
+//  Created by dc004 on 16/9/18.
+//  Copyright © 2016年 dc004. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
